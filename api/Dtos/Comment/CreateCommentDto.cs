a847d95a013a631d23fe9a45654a1d3d62b1ebbe
Varun2004Yadavgit config --global user.email <varun2004kkr@gmail.com>
2026-01-16 13:23:35 +0530

    Added_DataValidations

--- api/Dtos/Comment/CreateCommentDto.cs
+++ api/Dtos/Comment/CreateCommentDto.cs
@@ -1,10 +1,18 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace api.Dtos;
 
 public class CreateCommentDto
 {
+    [Required]
+    [MinLength(5,ErrorMessage ="Title must be 5 Characters")]
+    [MaxLength(280,ErrorMessage ="Title cannot be overr 280 characters")]
     public string Title {get; set;} = string.Empty;
+
+    [Required]
+    [MinLength(5,ErrorMessage ="Content must be 5 Characters")]
+    [MaxLength(280,ErrorMessage ="Content cannot be overr 280 characters")]
     public string Content {get; set;} = string.Empty;
     
 }
